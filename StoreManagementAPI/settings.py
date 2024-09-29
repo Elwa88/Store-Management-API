2010f27713b7743a5b2ee25dd7fd072fc98b9ecd
Elwa88 <fsociety.core@gmail.com>
2024-09-29 18:28:05 +0400

    created sales application

--- StoreManagementAPI/settings.py
+++ StoreManagementAPI/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'userauth',
     'warehouse',
+    'sales',
 ]
 
 MIDDLEWARE = [
--- StoreManagementAPI/urls.py
+++ StoreManagementAPI/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/',include('userauth.urls')),
+    path('api/auth/', include('userauth.urls')),
     path('api/warehouse/', include('warehouse.urls')),
+    path('api/sales/', include('sales.urls')),
 ]

72236039e9c11422dfd23c4e9ea5ee59fd1df1b9
Elwa88 <fsociety.core@gmail.com>
2024-09-29 18:39:33 +0400

    created Sale and Report models for Sales app

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from warehouse.models import Product
 
-# Create your models here.
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    date_sold = models.DateField(auto_now_add=True)
+    client_name = models.CharField(max_length=100)
+    client_personal_no = models.CharField(max_length=11, unique=True)
+    price_sold = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10,decimal_places=2)
+
+class GenerateReport(models.Model):
+    sale = models.ForeignKey(Sale,on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()

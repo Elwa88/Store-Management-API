864719c76d9f45bafcc5d50a998aebbbf6d948c6
Elwa88 <fsociety.core@gmail.com>
2024-10-06 15:43:03 +0400

    created Product and Feedback Tests

--- warehouse/tests.py
+++ warehouse/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
-from .models import Product, Category, Stock
+from .models import Product, Category, Stock, Supplier
 from rest_framework import status
 from rest_framework.test import APIClient
 from userauth.models import CustomUser
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class CategoryTests(TestCase):
+class CategorySupplierStockTests(TestCase):
     def setUp(self) :
         self.client = APIClient()
         self.admin_user = CustomUser.objects.create(username='user_admin',password = 'userpass', first_name ='name', last_name = "lname", role = 'admin')
@@ -108,3 +108,116 @@ class CategoryTests(TestCase):
         
         response2 = self.client.patch('/api/warehouse/stock/1/', {'product_name' : 'product2'})
         self.assertEqual(response2.status_code, status.HTTP_403_FORBIDDEN)
+
+class ProductFeedbackTests(TestCase):
+    def setUp(self) :
+        self.client = APIClient()
+        self.admin_user = CustomUser.objects.create(username='user_admin',password = 'userpass', first_name ='name', last_name = "lname", role = 'admin')
+        self.admin_token = RefreshToken.for_user(self.admin_user)
+        self.manager_user = CustomUser.objects.create(username='user_manager',password = 'userpass', first_name ='name', last_name = "lname", role = 'manager')
+        self.manager_token = RefreshToken.for_user(self.manager_user)
+        self.salesperson_user = CustomUser.objects.create(username='user_salesperson',password = 'userpass', first_name ='name', last_name = "lname", role = 'salesperson')
+        self.salesperson_token = RefreshToken.for_user(self.salesperson_user)
+
+    def test_product_views_admin_auth(self):
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.admin_token.access_token}')
+        category = Category.objects.create(**{"name" : "category1"})
+        self.assertEqual(len(Stock.objects.all()), 0)
+        response = self.client.post('/api/warehouse/product/',{
+                                                                "id": 1,
+                                                                "category": category.id,
+                                                                "name": "product1",
+                                                                "description": "descr.",
+                                                                "price_bought": "25.00",
+                                                                "price_selling": "40.00",
+                                                                "serial_number": "SN123456",
+                                                                "item_code": "ITEM1",
+                                                            })
+        self.product = Product.objects.get(pk = 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        stock = Stock.objects.get(product_name = "product1")
+        self.assertEqual(stock.quantity, 1)
+        response2 = self.client.get('/api/warehouse/product/')
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        self.assertLess(self.product.price_selling, 49)
+        response3 = self.client.patch('/api/warehouse/product/1/',{"price_selling": "50.00"})
+        self.assertEqual(response3.status_code, status.HTTP_200_OK)
+        self.product.refresh_from_db()
+        self.assertGreater(self.product.price_selling, 49)
+        response4 = self.client.delete('/api/warehouse/product/1/')
+        self.assertEqual(response4.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_product_views_other_auth(self):
+        category = Category.objects.create(**{"name" : "category1"})
+        product = Product.objects.create(**{
+                                    "id": 1,
+                                    "category": category,
+                                    "name": "product1",
+                                    "description": "descr.",
+                                    "price_bought": "25.00",
+                                    "price_selling": "40.00",
+                                    "serial_number": "SN123456",
+                                    "item_code": "ITEM1",
+                                })
+        #for salespeople
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.salesperson_token.access_token}')
+        response = self.client.post('/api/warehouse/product/',{
+                                                                "id": 1,
+                                                                "category": category.id,
+                                                                "name": "product1",
+                                                                "description": "descr.",
+                                                                "price_bought": "25.00",
+                                                                "price_selling": "40.00",
+                                                                "serial_number": "SN123456",
+                                                                "item_code": "ITEM1",
+                                                            })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response2 =self.client.get('/api/warehouse/product/')
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        response3 = self.client.get('/api/warehouse/product/1/')
+        self.assertEqual(response3.status_code, status.HTTP_200_OK)
+        response4 = self.client.delete('/api/warehouse/product/1/')
+        self.assertEqual(response4.status_code, status.HTTP_403_FORBIDDEN)
+
+        #for managers
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.manager_token.access_token}')
+        response = self.client.post('/api/warehouse/product/',{
+                                                                "id": 1,
+                                                                "category": category.id,
+                                                                "name": "product1",
+                                                                "description": "descr.",
+                                                                "price_bought": "25.00",
+                                                                "price_selling": "40.00",
+                                                                "serial_number": "SN123456",
+                                                                "item_code": "ITEM1",
+                                                            })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        response2 =self.client.get('/api/warehouse/product/')
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        response3 = self.client.get('/api/warehouse/product/1/')
+        self.assertEqual(response3.status_code, status.HTTP_200_OK)
+        response4 = self.client.delete('/api/warehouse/product/1/')
+        self.assertEqual(response4.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_feedback_views_admin_auth(self):
+        category = Category.objects.create(**{"name" : "category1"})
+        product = Product.objects.create(**{
+                                    "id": 1,
+                                    "category": category,
+                                    "name": "product1",
+                                    "description": "descr.",
+                                    "price_bought": "25.00",
+                                    "price_selling": "40.00",
+                                    "serial_number": "SN123456",
+                                    "item_code": "ITEM1",
+                                })
+        supplier = Supplier.objects.create(**{"name" : "supplier1", "email" : "supplier@email.test", "product_name" : "product1", "rating" : 5})
+        self.assertEqual(supplier.rating, 5)
+
+        self.client.credentials(HTTP_AUTHORIZATION = f'Bearer {self.admin_token.access_token}')
+        response = self.client.post('/api/warehouse/feedback/', {"id" : 1, "product" : product.id , "supplier" : supplier.id, "satisfaction" : "satisfied", "grade" : 2})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        supplier.refresh_from_db()
+        self.assertEqual(supplier.rating,7)
+        response1 = self.client.get('/api/warehouse/feedback/')
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
